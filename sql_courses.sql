3197c52323a615eaa755c365c9a847012f6e474b
kayreeder <kreeder2013@gmail.com>
2025-08-31 13:24:06 -0400

    Update sql_courses.sql

--- sql_courses.sql
+++ sql_courses.sql
@@ -1,5 +1,21 @@
 --Selecting movies released on even number years
 
-SELECT title, year
+SELECT title, 
+  year
 FROM movies
 WHERE year % 2 = 0;
+
+--finding total number of years employed by all engineers using having clause
+
+SELECT role, 
+  SUM(years_employed)
+FROM employees
+GROUP BY role
+HAVING role = "Engineer";
+
+--finding number of artists in studio without having clause
+
+SELECT role, 
+COUNT(*) as Number_of_artists
+FROM employees
+WHERE role = "Artist";
